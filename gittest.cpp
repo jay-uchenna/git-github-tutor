ef97ba475097f9f4cb31d96c7508687f08c96847
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-04-05 12:50:57 +0300

    second commit

--- gittest.cpp
+++ gittest.cpp
@@ -2,5 +2,11 @@
 
 int main(){
 	std::cout << "hello world\n";
+	int a, b;
+	std::cout <<"please enter a: ";
+	std::cin >> a;
+	std::cout <<"please enter b: ";
+	std::cin >> b;
+	std::cout <<"a + b = " << a+b <<"\n";
 	return 0;
 }
